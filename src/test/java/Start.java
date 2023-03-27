65d57c37f323f06c168ee015c2edcc4009f6acc4
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-27 23:24:36 +0200

    @Test successLogin

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Start {
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Start {
+
+    @Test
+    public void successLogin(){
+        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
 }
