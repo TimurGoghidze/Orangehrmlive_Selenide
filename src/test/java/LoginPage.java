1258e3cccc79efdc6a9b9beb8f054edf5eb5a52b
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-28 01:06:46 +0200

    info

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -9,25 +9,26 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    private SelenideElement usernameField =  $(byName("username"));
+    private SelenideElement usernameField = $(byName("username"));
     private SelenideElement passwordField = $(byCssSelector("[name='password']"));
-    private SelenideElement loginButton = $(byAttribute("type","submit"));
+    private SelenideElement loginButton = $(byAttribute("type", "submit"));
     private SelenideElement errorMessage = $(byCssSelector("[class = 'oxd-alert oxd-alert--error']"));
 
 
-    public void enterUsername(String usernameValue){
+    public void enterUsername(String usernameValue) {
         usernameField.shouldBe(visible); //обеспечиваем явное ожидание
         usernameField.setValue(usernameValue);//получим из тестов
     }
 
-    public void enterPassword(String passwordValue){
+    public void enterPassword(String passwordValue) {
         passwordField.setValue(passwordValue);//получим из тестов
     }
 
-    public void clickOnLoginButton(){
+    public void clickOnLoginButton() {
         loginButton.click();
     }
-public void checkErrorMessage(){
-    errorMessage.shouldHave(text("Invalid credentials"));
-}
+
+    public void checkErrorMessage() {
+        errorMessage.shouldHave(text("Invalid credentials"));//чтобы изъять данные текстовые
+    }
 }
