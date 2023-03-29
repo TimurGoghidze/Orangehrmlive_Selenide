c6c30e494be21f570488e0ab1ac9227e9a19239e
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-29 12:11:15 +0200

    add new pom.xml

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,23 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.After;
+import org.junit.Before;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
+
+    public final String BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+    LoginPage loginPage = new LoginPage();
+    ResetPasswordPage resetPasswordPage = new ResetPasswordPage();
+    DashBoardPage dashBoardPage = new DashBoardPage();
+    @Before
+    public void setUp(){
+        open(BASE_URL);
+        Configuration.fastSetValue = true; // для скорости
+    }
+
     @After
     public void tearDown() {
         closeWebDriver();
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,28 +1,20 @@
-
 import com.codeborne.selenide.Configuration;
 import org.junit.Test;
 
-import static com.codeborne.selenide.Selenide.open;
 
-public class LoginTests extends BaseTest{
+public class LoginTests extends BaseTest {
 
     @Test
     public void successLogin() {
-        Configuration.fastSetValue = true; //чтобы быстрее отрабатывало
-        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");//достаточно
-        LoginPage loginPage = new LoginPage();
         loginPage.enterUsername("Admin");
         loginPage.enterPassword("admin123");
         loginPage.clickOnLoginButton();
-        DashBoardPage dashBoardPage = new DashBoardPage();
         dashBoardPage.dashboardPageIsOpen();
     }
 
     @Test
     public void invalidLogin() {
-        Configuration.fastSetValue = true; //чтобы быстрее отрабатывало
-        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");//достаточно
-        LoginPage loginPage = new LoginPage();
+        Configuration.fastSetValue = true;
         loginPage.enterUsername("vdvd");
         loginPage.enterPassword("vdvd");
         loginPage.clickOnLoginButton();
@@ -31,16 +23,12 @@ public class LoginTests extends BaseTest{
 
     @Test
     public void elementsAreVisible1() {
-        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        LoginPage loginPage = new LoginPage();
         loginPage.checkImageLogo();
 
     }
 
     @Test
     public void elementsAreVisible2() {
-        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        LoginPage loginPage = new LoginPage();
         loginPage.logoIsDisplayed();
         loginPage.logoImageIsCorrect();
         loginPage.credSectionIsDisplayed();
@@ -48,10 +36,7 @@ public class LoginTests extends BaseTest{
 
     @Test
     public void forgotYourPasswordLink() {
-        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        LoginPage loginPage = new LoginPage();
         loginPage.followTheForgotYourPasswordLink();
-        ResetPasswordPage resetPasswordPage = new ResetPasswordPage();
         resetPasswordPage.setTitleResetPassword();
         //resetPasswordPage.urlIsCorrect();
         resetPasswordPage.URLisCorrectContainsDesiredPeace();
